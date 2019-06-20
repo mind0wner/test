3322c9b0d0e3c61491bde5ad554bcc1e820c393c
mind0wner <tasun.v.s@gmail.com>
2019-06-20 10:26:00 +0300

    clazz logic

--- src/com/abstr/Clazz.java
+++ src/com/abstr/Clazz.java
@@ -1,5 +1,32 @@
 package com.abstr;
 
+import java.util.Objects;
+
 public class Clazz {
     private String s;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clazz clazz = (Clazz) o;
+        return s.equals(clazz.s);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(s);
+    }
+
+    public Clazz(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
 }
