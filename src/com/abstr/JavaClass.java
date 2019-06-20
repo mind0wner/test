ae1cd80d4a5882b45651c3a2624bcaff987f64d8
mind0wner <tasun.v.s@gmail.com>
2019-06-20 13:21:35 +0300

    integer class

---
+++ src/com/abstr/JavaClass.java
@@ -0,0 +1,57 @@
+package com.abstr;
+
+public class JavaClass {
+    private Integer integer;
+    private Integer integer1;
+    private Integer integer2;
+    private Integer integer3;
+    private Integer integer4;
+
+    public JavaClass(Integer integer, Integer integer1, Integer integer2, Integer integer3, Integer integer4) {
+        this.integer = integer;
+        this.integer1 = integer1;
+        this.integer2 = integer2;
+        this.integer3 = integer3;
+        this.integer4 = integer4;
+    }
+
+    public Integer getInteger() {
+        return integer;
+    }
+
+    public void setInteger(Integer integer) {
+        this.integer = integer;
+    }
+
+    public Integer getInteger1() {
+        return integer1;
+    }
+
+    public void setInteger1(Integer integer1) {
+        this.integer1 = integer1;
+    }
+
+    public Integer getInteger2() {
+        return integer2;
+    }
+
+    public void setInteger2(Integer integer2) {
+        this.integer2 = integer2;
+    }
+
+    public Integer getInteger3() {
+        return integer3;
+    }
+
+    public void setInteger3(Integer integer3) {
+        this.integer3 = integer3;
+    }
+
+    public Integer getInteger4() {
+        return integer4;
+    }
+
+    public void setInteger4(Integer integer4) {
+        this.integer4 = integer4;
+    }
+}
--- src/com/abstr/RealClass.java
+++ src/com/abstr/RealClass.java
@@ -1,7 +1,7 @@
 package com.abstr;
 
 public class RealClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
 		int w = 3;
 		int e = 3;
        Child child = new Child();
